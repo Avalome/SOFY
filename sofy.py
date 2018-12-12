cac0513d2f769d76ee5d178ded57cbd824419a2e
Ange <s4nixd@gmail.com>
2018-12-12 16:47:15 +0100

    Creating and updating the core of Sofy

--- sofy.py
+++ sofy.py
@@ -12,16 +12,22 @@ import os
 def sofyPRINT(message):
     print "SF > "+message
 
+def presentation():
+    os.system("cls")
+    print "\n----------------------------------------------------------"
+    print "---------------------- S   O   F   Y ---------------------"
+    print "----------------------------------------------------------"
+    print "__________________________________By S@n1x-D4rk3r_________\n"
+    sofyPRINT("Welcome to SoFy, a software manager made for humans!")
+
+presentation()
 ## Checking the current OS
 if platform.system() == 'Windows':
-    sofyPRINT("OS detected: "+platform.platform())
-
+    sofyPRINT("OS detected: '"+platform.platform()+"'")
 elif platform.system() == 'Linux':
     sofyPRINT("OS detected: "+platform.platform())
-
 elif platform.system() == 'Mac':
     sofyPRINT("OS detected: "+platform.platform())
-
 else:
     sofyPRINT("SoFy is not yet configured for this kind of OS")
 
