72ff645b8fe16c198e0b57a8a9882776665551b2
Ange <s4nixd@gmail.com>
2018-12-12 17:06:42 +0100

    Adding the encryption/Decryption Method

--- sofy.py
+++ sofy.py
@@ -1,7 +1,9 @@
+#
 #################
 # SOFY Core
 # By Sanix Darker
 #################
+#
 
 # Imports
 import platform
@@ -9,9 +11,11 @@ import sys
 import os
 
 # Methods
+# Just for a specific print log
 def sofyPRINT(message):
     print "SF > "+message
 
+# The presentation/ Cleaning method
 def presentation():
     os.system("cls")
     print "\n----------------------------------------------------------"
@@ -19,6 +23,23 @@ def presentation():
     print "----------------------------------------------------------"
     print "__________________________________By S@n1x-D4rk3r_________\n"
     sofyPRINT("Welcome to SoFy, a software manager made for humans!")
+
+#
+# Encrypt/Decrypt Key From SOFY
+# Usage:
+# secret_data = "239054"
+# print sf_crypt_string(secret_data, encode=True)
+# print sf_crypt_string(sf_crypt_string(secret_data, encode=True), decode=True)
+#
+def sf_crypt_string(data, key='S@n1x-D4rk3r', encode=False, decode=False):
+    from itertools import izip, cycle
+    import base64
+    if decode:
+        data = base64.decodestring(data)
+    sfed = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(data, cycle(key)))
+    if encode:
+        return base64.encodestring(sfed).strip()
+    return sfed
 
 presentation()
 ## Checking the current OS
